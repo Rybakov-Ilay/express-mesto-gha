117dc6457056e0cce4a9acd44f861169afb2ccf2
Rybakov-Ilay <ilay106@mail.ru>
2022-08-01 12:13:23 +0300

    feat: sprint №13 Настройка .editorconfig, .eslintrc, .gitignore, установка express и hot-reload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`)
+})
+app.get('/', (req, res) => {
+  res.send(
+    `<html>
+        <body>
+            <p>Ответ на сигнал из космоса</p>
+        </body>
+        </html>`
+  );
+});
