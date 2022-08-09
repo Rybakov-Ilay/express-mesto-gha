efdd81de7b0a2af76c01a800428531ae24b8bf57
Rybakov-Ilay <ilay106@mail.ru>
2022-08-09 13:16:03 +0300

    refactor: app

--- utils/errors.js
+++ utils/errors.js
@@ -4,7 +4,7 @@ const ERROR_DEFAULT = 500;
 
 const ERROR_CODE_MESSAGE = { message: 'Переданы некорректные данные.' };
 const ERROR_NOT_FOUND_MESSAGE = { message: 'Запрашиваемый id не найден.' };
-const ERROR_DEFAULT_MESSAGE = { message: `Произошла ошибка: ${err.message}` }; // eslint-disable-line
+const ERROR_DEFAULT_MESSAGE = { message: `Произошла ошибка` }; // eslint-disable-line
 
 module.exports = {
   ERROR_CODE,
