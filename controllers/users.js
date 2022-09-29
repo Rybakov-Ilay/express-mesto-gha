61b1c8f4a6e5a4a5828f505db37dea40678b046e
Rybakov-Ilay <ilay106@mail.ru>
2022-09-29 21:32:07 +0300

    fix: тест №9 и снова куки

--- controllers/users.js
+++ controllers/users.js
@@ -72,7 +72,7 @@ module.exports.login = (req, res, next) => {
         sameSite: true,
       });
 
-      return res.send({ token: req.cookies.jwt });
+      return res.send({ token });
     })
     .catch(next);
 };
